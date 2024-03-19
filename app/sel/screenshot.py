23f77c4aee6df490bd7061ef160fa2489b9cab39
Skaeel <successdm13@gmail.com>
2024-03-19 17:53:10 +0500

    Added environment variables

--- app/sel/screenshot.py
+++ app/sel/screenshot.py
@@ -2,14 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import os
 
+
+se_event_bus_host = os.getenv('SE_EVENT_BUS_HOST')
 
 def make_screenshot(url: str) -> bytes:
     options = webdriver.FirefoxOptions()
     options.add_argument('--headless')
     options.add_argument('--disable-blink-features=AutomationControlled')
     driver = webdriver.Remote(
-        command_executor='http://selenium-hub:4444/wd/hub',
+        command_executor=f'http://{se_event_bus_host}:4444/wd/hub',
         options=options
     )
 
